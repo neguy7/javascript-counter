86645313901e3f131308560ff295390e9a073bd3
neguy7 <114348623+neguy7@users.noreply.github.com>
2022-11-06 15:50:44 +0100

    final update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,61 @@
-let add = document.querySelector("#add");
-let subtract = document.querySelector("#subtract");
+const heading = document.querySelector("h1");
 
-add.addEventListener("click", function () {
-  let output = document.querySelector("#output");
-  let result = Number(output.innerText) + 1;
+// counter element 
+const counterEl = document.createElement("div");
+counterEl.classList.add("counter");
+heading.after(counterEl);
 
-  if (result > 99) {
-    result = 0;
+// subtract button
+const subtract = document.createElement("button");
+subtract.setAttribute("title", "subtract one");
+counterEl.after(subtract);
+
+const subtractIcon = document.createElement("i");
+subtractIcon.classList.add("fa-solid", "fa-minus");
+subtract.append(subtractIcon);
+
+// reset button
+const reset = document.createElement("button");
+reset.classList.add("reset");
+reset.setAttribute("title", "Reset");
+reset.textContent = "Reset";
+subtract.after(reset);
+
+// add button
+const add = document.createElement("button");
+add.setAttribute("title", "add one");
+reset.after(add);
+
+const addIcon = document.createElement("i");
+addIcon.classList.add("fa-solid", "fa-plus");
+add.append(addIcon);
+
+// counter to 0
+let counter = 0;
+counterEl.textContent = counter;
+
+// event listeners 
+add.addEventListener("click", addOne);
+subtract.addEventListener("click", subtractOne);
+reset.addEventListener("click", resetCounter);
+
+// add function 
+function addOne() {
+  counter++;
+  counterEl.textContent = counter;
+}
+
+// subtract function
+function subtractOne() {
+  if (counter === 0) {
+    return;
   }
+  counter--;
+  counterEl.textContent = counter;
+}
 
-  output.innerText = result;
-});
-
-subract.addEventListener("click", function () {
-  let output = document.querySelector("#output");
-  let result = Number(output.innerText) - 1;
-
-  if (result < 0) {
-    result = 0;
-  }
-
-  output.innerText = result;
-});
-
-
-
+// Reset
+function resetCounter() {
+  counter = 0;
+  counterEl.textContent = counter;
+}
