3acb5e26621cbca8473decdfda22e0b1d562f87a
neguy7 <114348623+neguy7@users.noreply.github.com>
2022-11-06 01:34:15 +0100

    first

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) + 1;
+
+  if (result > 99) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+
+subract.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+
+
+
